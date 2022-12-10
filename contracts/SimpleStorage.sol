7624c0f8059e218e9cccc84baf25c79e24a023d0
fjoost <fjoostr@gmail.com>
2022-12-10 00:59:00 -0300

    struct, arrays & push multiple registers

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,27 +1,34 @@
 //SPDX-License-Identifier: MIT
 
-//pragma solidity ^0.8.7; the symbol declare that from this version of compiler on above the contracts works
-//pragma solidity >=0.8.7 <0.9.0; this contract will work with any version of compiler between the two defined previously
-
 pragma solidity ^0.8.7;
 
 contract SimpleStorage {
-    //content of contract, like a class
-    //uint256, int256, bool, string, address, bytes32 || uint type initializes to zero
 
+    /**** almacenar un uint256 -> almacenar un array de personas, que tengan 2 atributos ****/
+    
+    //crear type, con struct
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //crear un array del type Persona
+    People[] public people;
+    
+    //añadir personas al array
+    function addPeople(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name }));
+    }
+    
     uint256 public favoriteNumber;
 
     function store(uint256 _favoriteNumber) public{
         favoriteNumber = _favoriteNumber;
-        //if we call a retrieve(), it will spend gas
-        //because, we adding +1 instruction to execute in parent function
-        //retrieve();
     }
 
-    //view & pure functions when called alone, don't spend gas
-    //because, it doesnt change blockchain status, only read status
     function retrieve() public view returns (uint256){
         return favoriteNumber;
 
     }
+
 }
